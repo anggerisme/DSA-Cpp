3915e55816be2de3bd55fe34ddaa8aed021debc1
Angger Bayu Pangestu <kontak@angger.me>
2021-07-30 21:45:20 +0700

    Inserting elemen

--- Array/Array-ADT.cpp
+++ Array/Array-ADT.cpp
@@ -8,15 +8,25 @@ class Array{
         int size;
         int length;
         void display(Array arr);
+        void insert(int index, int value);
         void append(Array *arr, int value);
 };
 
-// -------------- Inserting elemen --------------------
+// -------------- Display elemen --------------------
 void Array::display(Array arr){
     cout << "Tampilkan elemen" << endl;
     for (int i = 0; i < arr.length; i++){
         cout << arr.A[i];
     }
+}
+
+// -------------- Inserting elemen --------------------
+void Array::insert(int index, int value){
+    for (int i = length; i > index;i--){
+        A[i] = A[i - 1];
+    }
+    A[index] = value;
+    length++;
 }
 
 // -------------- Append elemen --------------------
@@ -39,7 +49,7 @@ int main(){
     }
     arr.length = n;
 
-
+    arr.insert(4, 15);
     arr.append(&arr, 10);
     arr.display(arr);
 }
