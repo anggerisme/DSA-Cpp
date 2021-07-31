40aef93777af13a1be9b7669e7e65f70ebffc6b4
Angger Bayu Pangestu <kontak@angger.me>
2021-07-31 15:02:49 +0700

    Deleting elemen

--- Array/Array-ADT.cpp
+++ Array/Array-ADT.cpp
@@ -9,10 +9,12 @@ class Array{
         int length;
         void display(Array arr);
         void insert(int index, int value);
-        void append(Array *arr, int value);
+        void append(int value);
+        int Delete(int index);
+
 };
 
-// -------------- Display elemen --------------------
+// -------------- 1. Display elemen --------------------
 void Array::display(Array arr){
     cout << "Tampilkan elemen" << endl;
     for (int i = 0; i < arr.length; i++){
@@ -20,7 +22,7 @@ void Array::display(Array arr){
     }
 }
 
-// -------------- Inserting elemen --------------------
+// -------------- 2. Inserting elemen --------------------
 void Array::insert(int index, int value){
     for (int i = length; i > index;i--){
         A[i] = A[i - 1];
@@ -29,10 +31,27 @@ void Array::insert(int index, int value){
     length++;
 }
 
-// -------------- Append elemen --------------------
-void Array::append(Array *arr, int value){
-    if(arr->length<arr->size){
-        arr->A[length++] = value;
+// -------------- 3. Append elemen --------------------
+void Array::append(int value){
+    if(length<size){
+        A[length++] = value;
+    }
+}
+
+// -------------- 4. Deleting elemen --------------------
+int Array::Delete(int index){
+    int i;
+    int x=0;
+
+    if(index>=0 && index<length){
+        x = A[index]; // Index yang akan dihapus
+        for (i = index; i < length;i++){
+            A[i] = A[i + 1]; // Copy index terkini dengan nilai index yg ada di depanya
+            length--; // Kurangi panjang array (karena sudah dihapus)
+            
+            return x; // Nilai yg dihapus
+        }
+        return 0;
     }
 }
 
@@ -49,7 +68,9 @@ int main(){
     }
     arr.length = n;
 
-    arr.insert(4, 15);
-    arr.append(&arr, 10);
+    // arr.append(&arr, 10);
+    system("cls");
+    cout << arr.Delete(2) << endl;
+    arr.insert(2, 3);
     arr.display(arr);
 }
