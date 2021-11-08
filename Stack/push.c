6ea37f21cc53ef6128bd008ec647761b38c18777
Angger Bayu Pangestu <kontak@angger.me>
2021-11-08 21:05:18 +0700

    cd c:\Belajar\Programming\DSA\Stack\ ; if (True) { gcc stack.c -o stack } ; if (True) { .\stack }

--- Stack/push.c
+++ Stack/push.c
@@ -7,6 +7,8 @@ struct stack{
   int *s;
 };
 
+//////////////// 1. Pushing elemen //////////////////
+
 void push(struct stack *st, int x){
     if(st->top == st->size-1){
         printf("full");
@@ -17,17 +19,34 @@ void push(struct stack *st, int x){
     }
 }
 
+//////////////// 2. Deleting elemen //////////////////
+
+int pop(struct stack *st){
+    int x = -1;
+    if(st->top==-1){
+        printf("Stack is underflow");
+    }
+    else{
+        x = st->s[st->top--];
+        // st->top--;
+    }
+    return x;
+}
+
 int main()
 {
     struct stack st;
     st.top = -1;
     printf("Masukkan ukuran array :");
     scanf("%d", &st.size);
-    // ptr = (float*) malloc(100 * sizeof(float));
-    
+
     st.s = (int*) malloc(st.size);
-    push(&st, 18);
-    printf("%d", st.s[0]);
+    push(&st, 1);
+    push(&st, 2);
+    push(&st, 3);
+    push(&st, 4);
+    printf("%d", pop(&st));
+    // printf("%d", st.s[0]);
 
     return 0;
 }
